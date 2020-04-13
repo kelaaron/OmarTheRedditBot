a5fa4e14be2a089ed786df7f4a2989ece809f0d6
kelaaron <kelaaron@umich.edu>
2020-04-12 23:14:13 -0400

    successfully reads posts

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import praw
+import re
 
 reddit = praw.Reddit('Omar')
 
 subreddit = reddit.subreddit("cscareerquestions")
 
 for post in subreddit.hot(limit = 5):
+
+    if(re.search())
     print("Title= ", post.title)
     print("text= ", post.selftext)
     print("score= ", post.score)
