304a87ebdc0d350d3ae4478005300d65eafb97da
kelaaron <kelaaron@umich.edu>
2020-04-13 15:18:07 -0400

    can be tagged and run analytics

--- bot.py
+++ bot.py
@@ -1,14 +1,46 @@
 import praw
 import re
+import helper
 
 reddit = praw.Reddit('Omar')
 
-subreddit = reddit.subreddit("cscareerquestions")
+subreddit = reddit.subreddit("testingground4bots")
 
-for post in subreddit.hot(limit = 5):
+comments = subreddit.comments(limit=None)
 
-    if(re.search())
-    print("Title= ", post.title)
-    print("text= ", post.selftext)
-    print("score= ", post.score)
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~")
+keyphrase = "/u/omartheredditbot"
+
+with open("replied_comments.txt", "r") as f:
+    comms_replied_to = f.read()
+    comms_replied_to = comms_replied_to.split("\n")
+    comms_replied_to = list(filter(None, comms_replied_to)) # creates a list of comments
+
+for comment in comments:
+    #print(comment.body.lower())
+
+    if keyphrase in comment.body.lower(): # Check if someone tags Omar
+        print(comment.body.lower())
+        print(type(comment.body.lower()))
+
+        if comment.id not in comms_replied_to:
+
+            query = ""
+            commlist = comment.body.lower().split()
+            print(commlist)
+            for idx in range(len(commlist)):
+                print(commlist[idx])
+                if commlist[idx] == keyphrase and idx < len(commlist) - 1:
+                    query = commlist[idx + 1] # make query the next word
+                    print("QUERY = ", query)
+                    break
+            
+            if query != "": # found a word
+                late, old = helper.search_posts(subreddit, query)
+                print("FOUND WORD ", query, "WITH VALUES ", late, ", ", old)
+
+
+
+
+
+
+    
---
+++ helper.py
@@ -0,0 +1,31 @@
+import praw
+import re
+import datetime
+
+def search_posts(subreddit, query):
+    lately_count = 0
+    old_count = 0
+    lately_posts = 0
+    old_posts = 0
+    now = datetime.datetime.utcnow()
+    #now = float(now.strftime("%s"))
+    print(now)
+    # now = (now - datetime(1970,1,1, tzinfo=timezone.utc)) / timedelta(seconds=1)
+    for submission in subreddit.new(limit=None):
+        print(submission.created_utc)
+        print(submission.title)
+        print(submission.selftext)
+        if(now - datetime.timedelta(days=30) <= datetime.datetime.fromtimestamp(submission.created_utc)):
+            lately_posts += 1
+            if re.search(query, submission.title, re.IGNORECASE):
+                lately_count += 1
+            elif re.search(query, submission.selftext, re.IGNORECASE):
+                lately_count += 1
+        else:
+            old_posts += 1
+            if re.search(query, submission.title, re.IGNORECASE):
+                old_count += 1
+            elif re.search(query, submission.selftext, re.IGNORECASE):
+                old_count += 1
+    return lately_count / lately_posts, old_count / old_posts
+    
